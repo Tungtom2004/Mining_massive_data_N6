8d2d643b0b0e31b63c8e2597816b0a6557be8090
Tungtom2004 <buiminhtung01052004@gmail.com>
2026-03-17 15:13:11 +0000

    Update read dataset for check data

--- demo.py
+++ demo.py
@@ -1,1 +1,15 @@
-print('Hello World')
+from datasets import load_dataset
+ds = load_dataset(
+    "leonardoblas/us_election_2024_telegram_distilled",
+    split="train",
+    streaming=True,
+    token = ''
+)
+
+subset = []
+
+for i, row in enumerate(ds):
+    if row.get('message_id') == 6:
+        print(row)
+        break 
+
