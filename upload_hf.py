1135618cdcee20f6415bc9695eb2da0b23632fd0
Tungtom2004 <buiminhtung01052004@gmail.com>
2026-03-24 15:47:20 +0000

    Update code for data preprocessing v2

--- upload_hf.py
+++ upload_hf.py
@@ -8,8 +8,8 @@ login(os.getenv("HF_TOKEN"))
 api = HfApi()
 
 api.upload_large_folder(
-    folder_path = '/Volumes/workspace/default/telegram_project/processed/messages_clean_parts_from_db_channels10',
-    repo_id = 'Tungtom2004/mining_massive_data_N6',
+    folder_path = '/Volumes/workspace/default/telegram_project/processed/messages_clean_parts_from_db_channels11/',
+    repo_id = 'Tungtom2004/mining_massive_data_N6_channel11',
     repo_type = 'dataset'
 )
 
